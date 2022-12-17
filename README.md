51594b95c7b52dfe76bbe4a783b3dc121cc99472
bassem eloraby <BASEM12683@GMAIL.COM>
2022-12-17 09:05:00 +0300

    1

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+===================
+https://www.youtube.com/watch?v=QVokz2OrEy8&list=PLhDKyjgkNJZUrNyuR1qwOgLRNQFCclHT7&index=16
+00:23:40
+00:24
--- src/components/Crud.js
+++ src/components/Crud.js
@@ -1,36 +1,89 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 function Crud() {
-  
   const [lists, setList] = useState([
     { id: 1, name: 'hp' },
     { id: 2, name: 'lenovo' },
   ]);
+  const [updateState, setUpdateState] = useState(-1);
   return (
     <div className="container">
-    <AddList/>
-     <table className="table  mt-4 border">
-     {lists.map((current)=>(
-        <tr className='text-center border'>
-        <td className='border'>{current.id}</td>
-        <td className='border'>{current.name}</td>
-        <td className='border'>
-        <button className='edit'>Edit</button>
-        <button className='delete'>Delete</button>
-        </td>
-        </tr>
-     ))}
-     </table>
+      <AddList setList={setList} />
+      <form >
+        <table className="table  mt-4 border">
+          {lists.map((current) =>
+            updateState === current.id ? (
+              <EditList current={current} lists={lists} setList={setList} />
+            ) : (
+              <tr className="text-center border">
+                <td className="border">{current.id}</td>
+                <td className="border">{current.name}</td>
+                <td className="border">
+                  <button
+                    className="edit"
+                    onClick={() => handleSubmit(current.id)}
+                  >
+                    Edit
+                  </button>
+                  <button className="delete">Delete</button>
+                </td>
+              </tr>
+            )
+          )}
+        </table>
+      </form>
     </div>
   );
+  function handleSubmit(id) {
+    setUpdateState(id);
+  }
 }
-
-function AddList () {
- return(
-    <form>
-        <input type="text" name='name' placeholder='enter name'/>
-        <button type='submit'>Add</button>
+//edit
+function EditList({ current, lists, setList }) {
+  function handInput() {
+    const name = current.target.name;
+    const value = name.value;
+   const newlist= lists.map((li) => (li.id === current.id ? { ...li, name: value } : li))
+   setList(newlist)
+  }
+  return (
+    <tr>
+      <td className="border">{current.id}</td>
+      <td>
+        <input
+          type="text"
+          name="name"
+          onChange={handInput}
+          value={current.name}
+        />
+      </td>
+      <td>
+        <button type="submit">Update</button>
+      </td>
+    </tr>
+  );
+}
+//add
+function AddList({ setList }) {
+  const nameRef = useRef();
+  function handleSubmit(event) {
+    event.preventDefault();
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const name = event.target.elements.name.value;
+    const newlist = {
+      id: id,
+      name,
+    };
+    setList((prevList) => {
+      return prevList.concat(newlist);
+    });
+    nameRef.current.value = '';
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="enter name" ref={nameRef} />
+      <button type="submit">Add</button>
     </form>
- )   
+  );
 }
 export default Crud;
